ea4c9ec4497be70ded64dad2b0a1d89342059c94
SHIBA BISWAS <56774836+RootCyberjet@users.noreply.github.com>
2020-03-15 16:40:58 +0530

    Find smallest i such that xi is even

---
+++ Lab-5/lab5-10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,j,x[100],p;
+	printf("Enter the number of numbers to be entered : ");
+	scanf("%d",&n);
+	printf("Enter %d numbers : ",n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&x[i]);
+	}
+	for(j=1;j<=n;j++)
+	{
+		if(x[j]%2==0)
+		{
+			p=j;
+			j=n;
+		}
+	}
+	printf("The smallest i such that xi is even is = %d.",p);
+}
