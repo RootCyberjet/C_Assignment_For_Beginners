5d8c6e160ee2ba491e3d2c3758c41c04d912ace9
SHIBA BISWAS <56774836+RootCyberjet@users.noreply.github.com>
2020-03-25 13:42:38 +0530

    Sum of even digits

---
+++ Lab-6/mlab6-4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"flab6-4.h"
+void main()
+{
+	int n,s;
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	s=sed(n);
+	printf("The sum of even digits of %d is = %d.",n,s);
+}
