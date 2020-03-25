bb89bb61c3c7ad3f234a742a463869769bc4d4bc
SHIBA BISWAS <56774836+RootCyberjet@users.noreply.github.com>
2020-03-25 13:50:00 +0530

    Increment even digits by 1

---
+++ Lab-6/mlab6-10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"flab6-10.h"
+void main()
+{
+	int n,s;
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	s=ied(n);
+	printf("The required number = %d.\n",s);
+}
+/*
+Enter the number : 42336
+The required number = 53337.
+
+Enter the number : 97513
+The required number = 97513.
+
+Enter the number : 247537
+The required number = 357537.
+
+*/
